0ccec6912be0a0f530d123d4cb810b7461bde7bb
Godfrey <1245541558@qq.com>
2015-09-07 11:05:21 +0800

    add main to run

---
+++ src/HotelWorldClocksRunner.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Godfrey on 2015/9/7.
+ */
+public class HotelWorldClocksRunner {
+    public static void main(String [] args){
+        UtcTime utcTime = new UtcTime();
+        utcTime.attach("beijing",new CityClock(8));
+        utcTime.attach("london",new CityClock(0));
+        utcTime.attach("moscow",new CityClock(4));
+        utcTime.attach("sydney",new CityClock(10));
+        utcTime.attach("newYork",new CityClock(-5));
+
+        PhoneClock phoneClock = new PhoneClock(8);
+        phoneClock.setUtcTime(utcTime);
+        phoneClock.setLocalTime(9);
+
+        utcTime.printTimeOfAllClocks();
+    }
+}
